91b571ec4d66003732ec0c7490cb8975b9d44f87
Sharon Yang <sharonyyang0@gmail.com>
2026-02-21 09:36:12 -0500

    create service and repository tests

---
+++ src/test/java/event_scheduler_api/config/JpaConfigTest.java
@@ -0,0 +1,7 @@
+package event_scheduler_api.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JpaConfigTest {
+
+}
---
+++ src/test/java/event_scheduler_api/security/JwtRequestFilterTest.java
@@ -0,0 +1,7 @@
+package event_scheduler_api.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtRequestFilterTest {
+
+}
