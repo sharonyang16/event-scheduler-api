bfe7980a59a56ac8cace1d4363173910798e1d92
Sharon Yang <sharonyyang0@gmail.com>
2026-02-21 10:43:39 -0500

    create user data factory

--- src/test/java/event_scheduler_api/mapper/UserMapperTest.java
+++ src/test/java/event_scheduler_api/mapper/UserMapperTest.java
@@ -1,7 +1,23 @@
 package event_scheduler_api.mapper;
 
+import event_scheduler_api.dto.response.UserResponse;
+import event_scheduler_api.model.User;
+import event_scheduler_api.util.UserDataFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserMapperTest {
+    private final UserMapper userMapper = new UserMapper();
 
+    @Test
+    @DisplayName("toUserResponse should return UserResponse obj when given User is valid")
+    void toUserResponseValid() {
+        User user = UserDataFactory.createUser();
+
+        UserResponse userResponse = this.userMapper.toUserResponse(user);
+
+        assertEquals("Smith", userResponse.getLastName());
+    }
 }
---
+++ src/test/java/event_scheduler_api/util/UserDataFactory.java
@@ -0,0 +1,21 @@
+package event_scheduler_api.util;
+
+import event_scheduler_api.model.User;
+
+import java.util.UUID;
+
+public class UserDataFactory {
+    private UserDataFactory(){}
+
+    public static final UUID USER_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+    public static User createUser() {
+        User user = new User();
+        user.setId(USER_UUID);
+        user.setEmail("someone@email.com");
+        user.setPassword("password123");
+        user.setFirstName("John");
+        user.setLastName("Smith");
+
+        return user;
+    }
+}
